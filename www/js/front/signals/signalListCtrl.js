b5326d8ae81eb9dc518d94f94106a3635e87cf3b
Martin Kryl <martin@baoweb.cz>
2015-08-17 07:31:30 +0200

    vypis pomoci angularu

--- www/js/front/signals/signalListCtrl.js
+++ www/js/front/signals/signalListCtrl.js
@@ -1,7 +1,17 @@
 app.controller("signalListCtrl", function($scope, $http) {
 
     $http.get('/api/1.0/signals').success(function (responseData) {
-        $scope.signals = responseData;
+        $scope.signals = responseData.results;
         console.log($scope.signals);
     });
+
+});
+
+app.directive('signalListItem', function() {
+    return {
+        scope: {
+            signal : '='
+        },
+        templateUrl: '/js/front/signals/signal-list-item.html'
+    };
 });
