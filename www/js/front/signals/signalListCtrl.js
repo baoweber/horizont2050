946bfa232dee29e3d8e98a22ca7fae81695d2dc5
Martin Kryl <martin@baoweb.cz>
2015-08-20 17:26:06 +0200

    filters with checkboxes

--- www/js/front/signals/signalListCtrl.js
+++ www/js/front/signals/signalListCtrl.js
@@ -1,8 +1,10 @@
 app.controller("signalListCtrl", function($scope, $http) {
 
+    var _self = this;
+
     $scope.search = {
         'name' : '',
-        categories_id: '',
+        categories_id: {},
         challenges: ''
     };
 
@@ -13,33 +15,73 @@ app.controller("signalListCtrl", function($scope, $http) {
 
     $scope.advSearch = function (item) {
 
-        var returnValue =  true;
-
         if (!($scope.search.name === undefined || $scope.search.name == '' || item.name.toLowerCase().indexOf($scope.search.name.toLowerCase()) > -1 )) {
-            returnValue = false;
+            return false;
         }
 
-        if (!($scope.search.name === undefined || $scope.search.name == '' || item.title.toLowerCase().indexOf($scope.search.name.toLowerCase()) > -1 )) {
-            returnValue = false;
+        if ( !_self.filterOneInArray($scope.search.categories_id, item.categories_id) ) {
+            return false;
         }
 
-        if (!($scope.search.categories_id === undefined || $scope.search.categories_id === '' || $scope.search.categories_id == item.categories_id)) {
-            returnValue = false;
+        if ( !_self.filterMoreInArray($scope.search.challenges, item.challenges) ) {
+            return false;
         }
 
-        var challengesFilter = false;
-        angular.forEach(item.challenges, function(pdata, index) {
-            if (pdata.id == $scope.search.challenges) {
-                challengesFilter = true;
+        return true;
+    };
+
+    this.filterMoreInArray = function(array, needleArray) {
+
+        var needleCheck = false;
+        if(array === undefined) {
+            return true;
+        }
+        if (!_self.isCheckboxArray(array)) {
+            return true;
+        }
+        angular.forEach(needleArray, function(needle, key){
+            if (_self.checkCheckboxArray(array, needle.id)) {
+                needleCheck = true;
             }
         });
-
-        if(!( $scope.search.challenges === undefined || $scope.search.challenges === '' || challengesFilter == true)) {
-            returnValue = false;
+        if(needleCheck) {
+            return true;
         }
-
-        return returnValue;
+        return false;
     };
+
+    this.filterOneInArray = function(array, needle) {
+        if(array === undefined) {
+            return true;
+        }
+        if (!_self.isCheckboxArray(array)) {
+            return true;
+        }
+        if (_self.checkCheckboxArray(array, needle)) {
+            return true;
+        }
+        return false;
+    };
+
+    this.isCheckboxArray = function (array) {
+        var checked = false;
+        angular.forEach(array, function(value, key){
+            if (value) {
+                checked = true;
+            }
+        });
+        return checked;
+    };
+
+    this.checkCheckboxArray = function(array, needle) {
+        var show = false;
+        angular.forEach(array, function(value, key){
+            if (needle == key && value == true) {
+                show = true;
+            }
+        });
+        return show;
+    }
 
 });
 
