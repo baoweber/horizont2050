2d1781b4bb4b4b0bc5e01f49c82909a32ba53640
Martin Kryl <martin@baoweb.cz>
2015-08-25 15:42:59 +0200

    News edititng

---
+++ app/model/News.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Models;
+
+
+use Nette\Diagnostics\Debugger;
+
+class News extends \DivineModel
+{
+    /** @var \DibiConnection */
+    private $db;
+
+    public function __construct(\DibiConnection $connection)
+    {
+        // runnging parent construct
+        parent::__construct($connection);
+
+        // assigning table name including :pref:
+        $this->table = ':pref:pages';
+
+        // assigning connection
+        $this->db = $connection;
+
+        // defining schema
+        $this->schema = array(
+            'title'             => '%s',
+            'menutitle'         => '%s',
+            'perex'             => '%s',
+            'slug'              => '%s',
+            'text'              => '%s',
+            'show_in_menu'      => '%i',
+            'active'            => '%i',
+            'redirect'          => '%s'
+        );
+    }
+
+    public function getSingleBySlug($slug)
+    {
+        // getting all entries
+        return $this->db->fetch(
+            "SELECT *
+            FROM %n
+            WHERE `slug` =  %s",
+            $this->table, $slug
+        );
+    }
+
+    public function getMenuTitle($id) {
+
+        return $this->getField($id, 'menutitle');
+
+    }
+
+}
